f8f8d8ff46a1de28992d4151cfc075541fecc4bf
Sean Elwood <seanrelwood@gmail.com>
2024-07-03 13:24:08 -0500

    data entered into db

--- tornado/db-scripts/mn_weathergov_stats.py
+++ tornado/db-scripts/mn_weathergov_stats.py
@@ -13,20 +13,21 @@ cnx = mysql.connector.connect(user = sqlserver_config['USERNAME'], password = sq
                               host = sqlserver_config['IPADDRESS'], database = sqlserver_config['DATABASE'])
 cursor = cnx.cursor()
 
-add_mn_county = ("INSERT INTO county "
+def add_minnesota_counties_to_db():
+    add_mn_county = ("INSERT INTO county "
               "(countyID, county, state, geometry) "
               "VALUES (default,%(county)s, %(state)s, %(geometry)s)")
-
-gdf = gpd.read_file('../cb_2023_us_county_500k') # get geodataframe, source https://www2.census.gov/geo/tiger/GENZ2023/gdb/
-gdf = gdf[gdf.STATE_NAME == 'Minnesota']
-for_county_table = gdf[['NAME','STATE_NAME','geometry']]
-for index,row in for_county_table.iterrows():
-    db_insert_data = {
-        'county' : row['NAME'],
-        'state' : row['STATE_NAME'],
-        'geometry' : row['geometry'] # TypeError: Python 'polygon' cannot be converted to a MySQL type
-    }
-    cursor.execute(add_mn_county, db_insert_data)
-    cnx.commit()
-cursor.close()
-cnx.close()
+    gdf = gpd.read_file('../cb_2023_us_county_500k') # get geodataframe, source https://www2.census.gov/geo/tiger/GENZ2023/gdb/
+    gdf = gdf[gdf.STATE_NAME == 'Minnesota']
+    gdf = gdf.to_wkt() # convert all geometry columns to well known text for storage in db
+    for_county_table = gdf[['NAME','STATE_NAME','geometry']]
+    for index,row in for_county_table.iterrows():
+        db_insert_data = {
+            'county' : row['NAME'],
+            'state' : row['STATE_NAME'],
+            'geometry' : row['geometry']
+        }
+        cursor.execute(add_mn_county, db_insert_data)
+        cnx.commit()
+    cursor.close()
+    cnx.close()
