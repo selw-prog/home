f01e97c3177485c9c5a42be54c8ac3a7d929bfcc
Sean Elwood <seanrelwood@gmail.com>
2024-11-07 21:56:34 -0600

    chartjs working

--- flaskproj/flaskr/static/js/tornadoStats.js
+++ flaskproj/flaskr/static/js/tornadoStats.js
@@ -1,5 +1,6 @@
 function getTornadoStats() {
     const formData = new FormData();
+    const chartData = {};
     formData.append('stateSelect', document.getElementById('stateSelect').value);
     fetch('/api/tornadoStats', {
         method: 'POST',
@@ -8,32 +9,71 @@ function getTornadoStats() {
     .then(response => response.json())
     .then(data => {
         const table = document.getElementById('tornadoStatsTable');
-        // header
+        // table header
         const headerRow = table.querySelector('thead tr')
         if(headerRow) {
             headerRow.innerHTML = '';
-            Object.keys(data[0]).forEach((property) => console.log(property))
             Object.keys(data[0]).forEach((property) => {
-                const th = document.createElement('th');
-                th.textContent = property;
-                headerRow.appendChild(th); 
-            }) 
+                if(typeof property === 'string') {
+                    const th = document.createElement('th');
+                    th.textContent = property;
+                    headerRow.prepend(th);   
+                }
+                else {
+                    const th = document.createElement('th');
+                    th.textContent = property;
+                    headerRow.appendChild(th);
+                }
+            })
         }
-        // body
+        // table body
         const tbody = table.querySelector("tbody");
         if(tbody) {
             tbody.innerHTML = '';
             data.forEach(item => {
-                console.log(item)
                 const row = document.createElement('tr');
-                Object.values(item).forEach(property => {
-                    const cell = document.createElement('td');
-                    cell.textContent = property;
-                    row.appendChild(cell);
+                Object.entries(item).forEach(([key, value]) => {
+                    if(typeof value === 'string') {
+                        const cell = document.createElement('td');
+                        cell.textContent = value;
+                        row.prepend(cell);
+                    }
+                    else {
+                        const cell = document.createElement('td');
+                        cell.textContent = value;
+                        row.appendChild(cell);
+                        if(Object.keys(chartData).indexOf(key) > -1) {
+                            chartData[key] += value;
+                        }
+                        else {
+                            chartData[key] = value;
+                        }
+                        
+                    }
                 });
-                tbody.appendChild(row)
+                tbody.appendChild(row);
             });
         }
+        // chart
+        const ctx = document.getElementById('tornadoStatsChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data : {
+                labels: Object.keys(chartData),
+                datasets: [{
+                    label : 'Num Tornados',
+                    data: Object.values(chartData),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales : {
+                    y : {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     })
 
     .catch(error => {
