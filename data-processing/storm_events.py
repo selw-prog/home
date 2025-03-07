288b8b17c1447fab6e85c9d1a91a8b6bcec019c6
Sean Elwood <seanrelwood@gmail.com>
2025-03-07 15:17:08 -0600

    process dir, dump into .csv

--- data-processing/storm_events.py
+++ data-processing/storm_events.py
@@ -2,28 +2,33 @@ import logging
 import os
 import time
 import pandas as pd
+from pathlib import Path
 from tkinter import filedialog
 
 # Set up logging
 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 log_file = 'C:\\Users\\seanr\\OneDrive\\Documents\\Logs\\Python\\{time}_{file_name}.log'.format(time=time.strftime("%Y%m%d-%H%M%S"), file_name=os.path.basename(__file__))
 logging.basicConfig(level=logging.INFO, filename = log_file, format = log_format)
+time=time.strftime("%Y%m%d-%H%M%S") # Get the current time to reference in file names
 
 def process_chunk(chunk):
     # Filter out rows with 'Tornado' in the 'EVENT_TYPE' column
-    tornadoes = chunk[chunk['EVENT_TYPE'].str.contains('Tornado')]
+    tornadoes = chunk[chunk['EVENT_TYPE'].eq('Tornado')]
     return tornadoes
 
 # Select the file to process
-file_path = filedialog.askopenfilename()
-logging.info(file_path)
+file_dir = filedialog.askdirectory()
+logging.info('Processing files in directory {d}'.format(d = file_dir))
 
-# 
-chunk_size = 1000
-reader = pd.read_csv(file_path, chunksize=chunk_size)
-for i, chunk in enumerate(reader):
-    logging.info('Processing chunk {n}'.format(n = i+1))
-    data = process_chunk(chunk)
-    logging.info(data.head())
-    if i == 5: 
-        break
+# Process file in chunks and save to a new file
+header=True
+for file in os.listdir(file_dir):
+    logging.info('Processing file {f}'.format(f=file))
+    reader = pd.read_csv(Path(file_dir, file), chunksize=1000)
+    for i, chunk in enumerate(reader):
+        logging.info('Processing chunk {n}'.format(n = i+1))
+        data = process_chunk(chunk)
+        logging.info(data.head())
+        data.to_csv('C:\\Users\\seanr\\OneDrive\\Documents\\Home-Lab\\tornado_data\\stormevents-db\\processed\\{t}_tornadoes.csv'.format(t = time),
+                    mode='a', header=header)
+        header=False
