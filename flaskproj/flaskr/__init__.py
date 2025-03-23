2a978c8f6477fd00da5069d27d2b047c7642d314
Sean Elwood <seanrelwood@gmail.com>
2025-03-22 21:00:30 -0500

    adjustments for new database table

--- flaskproj/flaskr/__init__.py
+++ flaskproj/flaskr/__init__.py
@@ -1,10 +1,9 @@
 import os
-
+import pandas as pd
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import select, Integer, String
-from dotenv import dotenv_values
+from sqlalchemy import select, Integer, String, Column, Date
+from dotenv import dotenv_values 
 
 def create_app(test_config=None):
     # create and configure the app
@@ -17,39 +16,14 @@ def create_app(test_config=None):
     sqlserver_config = dotenv_values('C:\\Users\\seanr\\OneDrive\\Documents\\Home-Lab\\code\\flaskproj\\flaskr\\.env')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{user}:{pwd}@{host}/{database}'.format(user = sqlserver_config['USERNAME'], pwd = sqlserver_config['PASSWORD'], host = sqlserver_config['IPADDRESS'], database = sqlserver_config['DATABASE'])
     db = SQLAlchemy(app)
-
-    class minnesota(db.Model):
-        id = db.Column(db.Integer, primary_key = True)
-        county_name = db.Column(db.String(255), nullable = False)
-        county_state = db.Column(db.String(255), nullable = False)
-        num_tornados_2010 = db.Column(db.Integer, nullable = True)
-        num_tornados_2011 = db.Column(db.Integer, nullable = True)
-        num_tornados_2012 = db.Column(db.Integer, nullable = True)
-        num_tornados_2013 = db.Column(db.Integer, nullable = True)
-        num_tornados_2014 = db.Column(db.Integer, nullable = True)
-        num_tornados_2015 = db.Column(db.Integer, nullable = True)
-        num_tornados_2016 = db.Column(db.Integer, nullable = True)
-        num_tornados_2017 = db.Column(db.Integer, nullable = True)
-        num_tornados_2018 = db.Column(db.Integer, nullable = True)
-        num_tornados_2019 = db.Column(db.Integer, nullable = True)
-        num_tornados_2020 = db.Column(db.Integer, nullable = True)
-        num_tornados_2021 = db.Column(db.Integer, nullable = True)
-
-    class wisconsin(db.Model):
-        id = db.Column(db.Integer, primary_key = True)
-        county_name = db.Column(db.String(255), nullable = False)
-        county_state = db.Column(db.String(255), nullable = False)
-        num_tornados_2014 = db.Column(db.Integer, nullable = True)
-        num_tornados_2015 = db.Column(db.Integer, nullable = True)
-        num_tornados_2016 = db.Column(db.Integer, nullable = True)
-        num_tornados_2017 = db.Column(db.Integer, nullable = True)
-        num_tornados_2018 = db.Column(db.Integer, nullable = True)
-        num_tornados_2019 = db.Column(db.Integer, nullable = True)
-        num_tornados_2020 = db.Column(db.Integer, nullable = True)
-        num_tornados_2021 = db.Column(db.Integer, nullable = True)
-        num_tornados_2022 = db.Column(db.Integer, nullable = True)
-        num_tornados_2023 = db.Column(db.Integer, nullable = True)
-        num_tornados_2024 = db.Column(db.Integer, nullable = True)
+    # tornado (id INT, event_date DATE, state VARCHAR(100), county VARCHAR(100), scale VARCHAR(3), location VARCHAR(100)) - PK id
+    class Tornado(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        event_date = db.Column(db.Date)
+        state = db.Column(db.String(100))
+        county = db.Column(db.String(100))
+        scale = db.Column(db.String(3))
+        location = db.Column(db.String(100))
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -76,55 +50,9 @@ def create_app(test_config=None):
     def dnsmonitor():
         return render_template('dnsmonitor.html')
 
-    @app.route('/api/mnTornadoStats', methods = ['POST'])
-    def mnQueryTornadoDB():
-        result = db.session.execute(db.select(minnesota).filter_by(county_state = request.form['stateSelect']))
-        data = []
-        for row in result:
-            row_as_dict = row._mapping
-            stats = {}
-            stats = {
-                'County Name' : row_as_dict['minnesota'].county_name,
-                'County State' : row_as_dict['minnesota'].county_state,
-                '2010' : row_as_dict['minnesota'].num_tornados_2010,
-                '2011' : row_as_dict['minnesota'].num_tornados_2011,
-                '2012' : row_as_dict['minnesota'].num_tornados_2012,
-                '2013' : row_as_dict['minnesota'].num_tornados_2013,
-                '2014' : row_as_dict['minnesota'].num_tornados_2014,
-                '2015' : row_as_dict['minnesota'].num_tornados_2015,
-                '2016' : row_as_dict['minnesota'].num_tornados_2016,
-                '2017' : row_as_dict['minnesota'].num_tornados_2017,
-                '2018' : row_as_dict['minnesota'].num_tornados_2018,
-                '2019' : row_as_dict['minnesota'].num_tornados_2019,
-                '2020' : row_as_dict['minnesota'].num_tornados_2020,
-                '2021' : row_as_dict['minnesota'].num_tornados_2021
-            }
-            data.append(stats)
-        return data
-
-    @app.route('/api/wiTornadoStats', methods = ['POST'])
-    def wiQueryTornadoDB():
-        result = db.session.execute(db.select(wisconsin).filter_by(county_state = request.form['stateSelect']))
-        data = []
-        for row in result:
-            row_as_dict = row._mapping
-            stats = {}
-            stats = {
-                'County Name' : row_as_dict['wisconsin'].county_name,
-                'County State' : row_as_dict['wisconsin'].county_state,
-                '2014' : row_as_dict['wisconsin'].num_tornados_2014,
-                '2015' : row_as_dict['wisconsin'].num_tornados_2015,
-                '2016' : row_as_dict['wisconsin'].num_tornados_2016,
-                '2017' : row_as_dict['wisconsin'].num_tornados_2017,
-                '2018' : row_as_dict['wisconsin'].num_tornados_2018,
-                '2019' : row_as_dict['wisconsin'].num_tornados_2019,
-                '2020' : row_as_dict['wisconsin'].num_tornados_2020,
-                '2021' : row_as_dict['wisconsin'].num_tornados_2021,
-                '2022' : row_as_dict['wisconsin'].num_tornados_2022,
-                '2023' : row_as_dict['wisconsin'].num_tornados_2023,
-                '2024' : row_as_dict['wisconsin'].num_tornados_2024
-            }
-            data.append(stats)
-        return data
+    @app.route('/api/getTornadoStats', methods = ['POST'])
+    def getTornadoStats():
+        result = pd.read_sql_query(sql = 'SELECT * FROM tornado WHERE state = "{s}"'.format(s = request.form['stateSelect'].upper()), con = db.engine)
+        return result.to_json(orient='records')
 
     return app
--- flaskproj/flaskr/static/js/tornadoStats.js
+++ flaskproj/flaskr/static/js/tornadoStats.js
@@ -19,8 +19,8 @@ function isFiltered() { // return true if there is a row with the filtered attri
 
 function toggleClickHandler(event, chart) {
     const row = event.target.closest('tr');
-    if(isFiltered()) {
-        if(row.hasAttribute('data-filtered')) { // clicked on row that is already filtered - reset to default
+    if (isFiltered()) {
+        if (row.hasAttribute('data-filtered')) { // clicked on row that is already filtered - reset to default
             row.removeAttribute('data-filtered');
             row.style.removeProperty('background-color');
             filterChart(defaultChartData, chart);
@@ -47,7 +47,7 @@ function filterOnTableRow(tableRow, chart) { // set attribute, change background
     const countyData = allData.find(item => item['County Name'] === category);
     const filteredCountyData = [];
     Object.entries(countyData).forEach(([key, value]) => {
-        if(chart.data.labels.includes(key)) {
+        if (chart.data.labels.includes(key)) {
             filteredCountyData[key] = value;
         }
     });
@@ -58,10 +58,10 @@ function createChart() { // init chart
     const ctx = document.getElementById('tornadoStatsChart').getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'line',
-        data : {
+        data: {
             labels: [],
             datasets: [{
-                label : 'Tornado Count',
+                label: 'Tornado Count',
                 data: [],
                 borderWidth: 1
             }]
@@ -86,8 +86,8 @@ function createChart() { // init chart
                     }
                 }
             },
-            scales : {
-                y : {
+            scales: {
+                y: {
                     beginAtZero: true
                 }
             }
@@ -126,55 +126,72 @@ function getTornadoStats() { // query database for state data, rewrite table, up
     const formData = new FormData();
     let headers = [];
     formData.append('stateSelect', document.getElementById('stateSelect').value);
-    switch(formData.get('stateSelect')) {
-        case 'Minnesota':
-            api = '/api/mnTornadoStats'
-            break;
-        case 'Wisconsin':
-            api = '/api/wiTornadoStats'
-            break;
-    }
-    fetch(api, {
+    fetch('/api/getTornadoStats', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json())
-    .then(data => {
-        allData = data;
-        const table = document.getElementById('tornadoStatsTable');
-        const regex = new RegExp('\\d+'); // regex for checking if property only contains numbers
-        // table header
-        const countyProperties = ['County Name', 'County State']
-        const years = []
-        const headerRow = table.querySelector('thead tr')
-        if(headerRow) {
-            headerRow.innerHTML = '';
-            Object.keys(data[0]).forEach((property) => {
-                if(regex.test(property)) {
-                    years.push(property)
+        .then(response => response.json())
+        .then(data => {
+            let years = []
+            // Add year property to every item in data
+            for (let i = 0; i < data.length; i++) {
+                let date = new Date(data[i].event_date)
+                data[i].year = date.getFullYear()
+                if (years.indexOf(data[i].year) === -1) { // Store years for table header
+                    years.push(data[i].year)
                 }
-            })
+            }
+            let countyStatsPerYear = []
+            // Get total count for each county per year, this will be used to write the table
+            for (let i = 0; i < data.length; i++) {
+                let countyIndex = countyStatsPerYear.findIndex(county => county['County Name'] === data[i].county)
+                if (countyIndex > -1) { // Found county in array
+                    if (countyStatsPerYear[countyIndex][data[i].year]) { // If year property exists, increment count
+                        countyStatsPerYear[countyIndex][data[i].year] += 1
+                    }
+                    else { // Add year property
+                        countyStatsPerYear[countyIndex][data[i].year] = 1
+                    }
+                }
+                else { // Add county to array
+                    let newCounty = { "County Name": data[i].county, "County State": data[i].state }
+                    newCounty[data[i].year] = 1 // initialize tornado count for year
+                    countyStatsPerYear.push(newCounty)
+                }
+            }
+            // Add any missing years to county in countyStatsPerYear, set to 0
+            for (let i = 0; i < countyStatsPerYear.length; i++) {
+                for (let yearIndex = 0; yearIndex < years.length; yearIndex++) {
+                    if (!Object.hasOwn(countyStatsPerYear[i], years[yearIndex])) {
+                        countyStatsPerYear[i][years[yearIndex]] = 0
+                    }
+                }
+            }
+            console.log(countyStatsPerYear)
+            const table = document.getElementById('tornadoStatsTable');
+            // Write table header
+            const countyProperties = ['County Name', 'County State']
+            const headerRow = table.querySelector('thead tr')
+            headerRow.innerHTML = '';
             headers = countyProperties.concat(years.sort(), 'Total')
             headers.forEach(header => {
                 const th = document.createElement('th');
                 th.textContent = header;
                 headerRow.appendChild(th);
             })
-        }
-        // table body
-        const tbody = table.querySelector("tbody");
-        if(tbody) {
+            // Write table body
+            const tbody = table.querySelector("tbody");
             tbody.innerHTML = '';
-            data.forEach(item => {
+            countyStatsPerYear.forEach(item => {
                 const row = document.createElement('tr');
                 var countyTotal = 0;
                 headers.forEach(header => {
-                    if(header != 'Total') { // skip header cell
+                    if (header != 'Total') { // skip header cell
                         const cell = document.createElement('td');
                         cell.textContent = item[header];
                         row.appendChild(cell);
-                        if(years.indexOf(header) > -1) { // calculate total tornados per county
-                            if(Object.keys(defaultChartData).indexOf(header) > -1) {
+                        if (years.indexOf(header) > -1) { // Calculate total tornados per county
+                            if (Object.hasOwn(defaultChartData,header)) {
                                 defaultChartData[header] += item[header];
                             }
                             else {
@@ -190,31 +207,31 @@ function getTornadoStats() { // query database for state data, rewrite table, up
                 row.appendChild(cell);
                 tbody.appendChild(row);
             });
-        }
-        // chart
-        const canvas = document.getElementById('tornadoStatsChart');
-        const myChart = Chart.getChart(canvas);
-        updateChart(defaultChartData, myChart)
-        // add sort listener to table columns
-        const headerColumns = table.querySelectorAll('th');
-        headerColumns.forEach((header, index) => { // add sorting listener to each column header
-            header.addEventListener('click', () => {
-                header.dataset.sort = header.dataset.sort === 'asc' ? 'desc' : 'asc';
-                sortTableByColumn(table.id, index, header.dataset.sort === 'asc');
+            // chart
+            console.log(defaultChartData)
+            const canvas = document.getElementById('tornadoStatsChart');
+            const myChart = Chart.getChart(canvas);
+            updateChart(defaultChartData, myChart)
+            // add sort listener to table columns
+            const headerColumns = table.querySelectorAll('th');
+            headerColumns.forEach((header, index) => { // add sorting listener to each column header
+                header.addEventListener('click', () => {
+                    header.dataset.sort = header.dataset.sort === 'asc' ? 'desc' : 'asc';
+                    sortTableByColumn(table.id, index, header.dataset.sort === 'asc');
+                });
             });
+            // add filter listener to table rows
+            const rows = table.getElementsByTagName('tr');
+            for (i = 1; i < rows.length; i++) { // start at 1 index to skip header
+                rows[i].addEventListener('click', (event) => {
+                    toggleClickHandler(event, myChart)
+                })
+            }
+            allData = countyStatsPerYear
+        })
+        .catch(error => {
+            console.log('error', error)
         });
-        // add filter listener to table rows
-        const rows = table.getElementsByTagName('tr');
-        for(i = 1; i < rows.length; i++) { // start at 1 index to skip header
-            rows[i].addEventListener('click', (event) => {
-                toggleClickHandler(event, myChart)
-            })
-        }
-    })
-
-    .catch(error => {
-        console.log('error', error)
-    })
 }
 
 createChart()
